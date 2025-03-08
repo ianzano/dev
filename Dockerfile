99b142e3dc152bc72c74c6b84fabbaf1901e9e4f
ani <ani@anto.cc>
2025-03-08 16:05:02 +0100

    fix

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,11 @@ USER ${USER}
 VOLUME /data
 WORKDIR /data
 
-RUN apt-get update && apt-get install -y git zip unzip zlib1g-dev libzip-dev && pecl install xdebug && docker-php-ext-install zip && docker-php-ext-enable xdebug
+RUN apt-get update && apt-get install -y git zip unzip zlib1g-dev libzip-dev nodejs npm && pecl install xdebug && docker-php-ext-install zip && docker-php-ext-enable xdebug
 
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
-COPY docker-entrypoint.sh /docker-entrypoint.sh
+COPY --chown=$USER:$GROUP docker-entrypoint.sh /docker-entrypoint.sh
 RUN chmod +x /docker-entrypoint.sh
 
 ENTRYPOINT [ "/docker-entrypoint.sh" ]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,27 +2,32 @@
 
 DATA_DIR="/data"
 
-if [ ! $(getent group $GROUP) ]; then
-    groupadd $GROUP
-    echo "%$GROUP ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+if [ $INIT == 'yes' ]; then
+  if [ ! $(getent group $GROUP) ]; then
+      groupadd $GROUP
+      echo "%$GROUP ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+  fi
+
+  if [ ! $(getent passwd $USER) ]; then
+      useradd -m -g "$GROUP" -d "/home/$USER" "$USER"
+      echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+  fi
+
+  chown -R $USER:$GROUP "$DATA_DIR"
+  chown $USER:$GROUP $COMPOSER_HOME
+  su $USER
+
+  for REPO in $REPOS; do
+      git config --global --add safe.directory "$DATA_DIR/$REPO"
+      [ -d "$DATA_DIR/$REPO" ] && git -C "$REPO" pull || git clone "https://github.com/dianaphp/$REPO.git"
+  done
+
+  for REPO in $REPOS; do
+      [ -f "$DATA_DIR/$REPO/composer.json" ] && composer install --working-dir="$DATA_DIR/$REPO"
+      [ -f "$DATA_DIR/$REPO/package.json" ] && npm install --prefix "$DATA_DIR/$REPO"
+  done
+
+  INIT=no
 fi
 
-if [ ! $(getent passwd $USER) ]; then
-    useradd -m -g "$GROUP" -d "/home/$USER" "$USER"
-    echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-fi
-
-chown -R $USER:$GROUP "$DATA_DIR"
-chown $USER:$GROUP $COMPOSER_HOME
-su $USER
-
-for REPO in $REPOS; do 
-    git config --global --add safe.directory "$DATA_DIR/$REPO"
-    [ -d "$DATA_DIR/$REPO" ] && git -C "$REPO" pull || git clone "https://github.com/dianaphp/$REPO.git"
-done
-
-for REPO in $REPOS; do 
-    composer install --working-dir="$DATA_DIR/$REPO"
-done
-
-php -S 0.0.0.0:8000 "$DATA_DIR/$SERVE"
+npm run --prefix "$DATA_DIR/$SERVE" dev -- --host
